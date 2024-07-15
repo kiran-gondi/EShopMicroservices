46798ba80669c81837a5c27a4102677ceb3eedab
kiran.gondi <kiran.gondi@planview.com>
2024-07-15 23:10:04 +0530

    Section-7 till 95 - Seeding CatalogDb with Marten Initial Baseline Data (IInitialData)

--- src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -1,30 +1,56 @@
-﻿namespace Catalog.Api.Products.CreateProduct
+﻿namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, 
+  string Description, string ImageFile, decimal Price)
+  : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
 {
-  public record CreateProductCommand(string Name, List<string> Category, 
-    string Description, string ImageFile, decimal Price)
-    : ICommand<CreateProductResult>;
-
-  public record CreateProductResult(Guid Id);
-  internal class CreateProductCommandHandler(IDocumentSession session) : 
-    ICommandHandler<CreateProductCommand, CreateProductResult>
-  {
-    public async Task<CreateProductResult> Handle(CreateProductCommand command, 
-      CancellationToken cancellationToken)
+    public CreateProductCommandValidator()
     {
-      //create product entity from command object
-      var product = new Product { 
-        Name = command.Name, 
-        Category = command.Category,
-        Description = command.Description, 
-        ImageFile = command.ImageFile,
-        Price = command.Price,
-      };
-
-      session.Store(product);
-      await session.SaveChangesAsync(cancellationToken);
-
-      //return CreateProductResult result
-      return new CreateProductResult(product.Id);
+      RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+      RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+      RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+      RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
     }
+}
+
+internal class CreateProductCommandHandler(IDocumentSession session) : 
+  ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+  public async Task<CreateProductResult> Handle(CreateProductCommand command, 
+    CancellationToken cancellationToken)
+  {
+    //Manual Validator
+    //await CreateProductCommandValidator(validator, command, cancellationToken);
+    
+    //create product entity from command object
+    var product = new Product
+    {
+      Name = command.Name,
+      Category = command.Category,
+      Description = command.Description,
+      ImageFile = command.ImageFile,
+      Price = command.Price,
+    };
+
+    session.Store(product);
+    await session.SaveChangesAsync(cancellationToken);
+
+    //return CreateProductResult result
+    return new CreateProductResult(product.Id);
   }
+
+  //private static async Task CreateProductCommandValidator(IValidator<CreateProductCommand> validator, CreateProductCommand command, CancellationToken cancellationToken)
+  //{
+  //  var result = await validator.ValidateAsync(command, cancellationToken);
+  //  var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+
+  //  if (errors.Any())
+  //  {
+  //    throw new ValidationException(errors.FirstOrDefault());
+  //  }
+  //}
 }
--- src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsHandler.cs
@@ -5,14 +5,12 @@
 
   public record GetProductsResult(IEnumerable<Product> Products);
 
-  internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) :
+  internal class GetProductsQueryHandler(IDocumentSession session) :
     IQueryHandler<GetProductsQuery, GetProductsResult>
   {
     public async Task<GetProductsResult> Handle(GetProductsQuery getProductsQuery, 
       CancellationToken cancellationToken)
     {
-      logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", getProductsQuery);
-
       var products  = await session.Query<Product>().ToListAsync(cancellationToken);
 
       return new GetProductsResult(products);
