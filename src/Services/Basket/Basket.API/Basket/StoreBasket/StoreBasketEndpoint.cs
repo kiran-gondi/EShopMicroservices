0cd69620b94dc4e9277e8abed90a2f861db1ef9c
kiran.gondi <kiran.gondi@planview.com>
2024-07-19 15:34:17 +0530

    Section-8 - Completed

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Basket.API.Basket.StoreBasket;
+public record StoreBasketRequest(ShoppingCart Cart);
+public record StoreBasketResponse(string UserName);
+
+public class StoreBasketEndpoint : ICarterModule
+{
+  public void AddRoutes(IEndpointRouteBuilder app)
+  {
+    app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) => {
+      var command = request.Adapt<StoreBasketCommand>();
+      var result = await sender.Send(command);
+      var response = result.Adapt<StoreBasketResponse>();
+      return Results.Created($"/basket/{response.UserName}", response);
+    })
+      .WithName("StoreBasketRequest")
+      .Produces<StoreBasketResponse>(StatusCodes.Status200OK)
+      .ProducesProblem(StatusCodes.Status400BadRequest)
+      .WithSummary("Store Basket Request By UserName")
+      .WithDescription("Store Basket Request By UserName");
+  }
+}
---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidator()
+    {
+      RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+      RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class StoreBasketCommandHandler(IBasketRepository basketRepository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+  public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+  {
+    ShoppingCart cart = command.Cart;
+    //TODO: store basket in DB (user Marten upsert - if exits = update, if not = create)
+    //TODO: update cache (Redis Distributed Cache)
+    await basketRepository.StoreBasket(command.Cart, cancellationToken);
+
+    return new StoreBasketResult(command.Cart.UserName);
+  }
+}
