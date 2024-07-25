b37c6b966e5d45d833b139d901f5dde43893fbee
kiran.gondi <kiran.gondi@planview.com>
2024-07-25 23:47:35 +0530

    Section-14 Ordering.Domain with Tactial DDD

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record OrderName
+{
+    private const int DefaultLength = 5;
+    public string Value { get; }
+    private OrderName(String value) => Value = value;
+
+    public static OrderName Of(String value)
+    {
+      ArgumentNullException.ThrowIfNull(value);
+      ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength);
+
+      return new OrderName(value);
+    }
+}
